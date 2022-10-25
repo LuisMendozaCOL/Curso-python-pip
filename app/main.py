8b2eb22dfe9739475f59f6be89c93860ebc5ef45
LuisMendozaCol <lamendozac@unal.edu.co>
2022-10-25 16:33:26 -0500

    web-server

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
 import charts
 import utils
 import read_csv
+import pandas as pd
 
 
 def run():
-  data = read_csv.read_csv('data.csv')
   country = input("Escriba el país: ")
-  #country = "Colombia"
+  
+  df = pd.read_csv('data.csv')
+  region = 'South America'
+  df = df[df['Continent'] == region]
+  countries = df['Country'].values
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart('region', countries, percentages)
+  
+  data = read_csv.read_csv('data.csv')
   country_data = utils.filter_by_country(data, country)
   labels, values = utils.get_population(country_data)
   charts.generate_bar_chart(country, labels, values)
+  
+  
+
 
 
 if __name__ == '__main__':
